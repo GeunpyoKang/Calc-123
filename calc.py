1f03d8e4e62ce712f203df4e94c8061227c6ac35
sanghyung.kim <sh03.kim@samsung.com>
2024-07-10 16:51:36 +0900

    [feature] getDivide

--- calc.py
+++ calc.py
@@ -1,3 +1,10 @@
 class Calc:
     def getGop(self, op1, op2):
         return op1 * op2
+
+    def getDivide(self, op1, op2):
+        try:
+            return op1 // op2
+        except ZeroDivisionError:
+            return "ZeroDivisionError"
+
--- test_calc.py
+++ test_calc.py
@@ -7,3 +7,11 @@ class TestCalc(TestCase):
     def test_gop(self):
         calc = Calc()
         self.assertEqual(calc.getGop(2, 3), 6)
+
+    def test_getDivide(self):
+        calc = Calc()
+        self.assertEqual(calc.getDivide(6, 2), 3)
+
+    def test_getDivide_zeroDivision(self):
+        calc = Calc()
+        self.assertEqual(calc.getDivide(6, 0), "ZeroDivisionError")
