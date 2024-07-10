3e642c006a0a9e70c6a2e89bc2464ba22c0a8470
hodri <kanggeunpyo@gmail.com>
2024-07-10 16:50:01 +0900

    [Feature] get_sum_sum의 test code 작성

--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,16 @@
 from unittest import TestCase
 
+from calc import Calc
+
 
 class TestCalc(TestCase):
-    pass
+
+    def setUp(self):
+        self.sut = Calc()
+
+    def test_invalid_params_in_get_sum_sum(self):
+        with self.assertRaises(AttributeError):
+            self.sut.get_sum_sum('a', 'b', 'c')
+
+    def test_get_sum_sum(self):
+        self.assertEqual(self.sut.get_sum_sum(1, 2, 3), 6)
