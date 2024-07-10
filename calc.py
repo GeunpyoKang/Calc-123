a28fe941a51ec43a9950fa399246093160ffd47f
hyowon.moon <hyowon.moon@samsung.com>
2024-07-10 16:52:48 +0900

    [feature] gop, zegop

--- calc.py
+++ calc.py
@@ -1,2 +1,7 @@
 class Calc:
-    pass
+
+    def getGop(self, a, b):
+        return a * b
+
+    def getZegop(self, a):
+        return a * a
--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,26 @@
 from unittest import TestCase
 
+from calc import Calc
+
 
 class TestCalc(TestCase):
-    pass
+
+    def test_gop_case(self):
+        calc = Calc()
+        ret = calc.getGop(2, 5)
+        self.assertEqual(ret, 10)
+        ret = calc.getGop(2, 7)
+        self.assertEqual(ret, 14)
+        ret = calc.getGop(28, 5)
+        self.assertEqual(ret, 140)
+        ret = calc.getGop(13, 3)
+        self.assertEqual(ret, 39)
+
+    def test_zegop_case(self):
+        calc = Calc()
+        ret = calc.getZegop(2)
+        self.assertEqual(ret, 4)
+        ret = calc.getZegop(3)
+        self.assertEqual(ret, 9)
+        ret = calc.getZegop(12)
+        self.assertEqual(ret, 144)
