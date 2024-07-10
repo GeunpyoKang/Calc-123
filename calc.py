1215f6a3a1d16b053c9342fbdeae59dba949e867
hodri <kanggeunpyo@gmail.com>
2024-07-10 16:51:56 +0900

    [Refactoring] python convention

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 class Calc:
     def get_sum_sum(self, a, b, c):
-        if type(a) != int or type(b) != int or type(c) != int:
+        if type(a) is not int or type(b) is not int or type(c) is not int:
             raise AttributeError
 
         return a + b + c
