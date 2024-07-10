c3bc982dc1636fb1ef5748ebaa987d642f6544dc
hodri <kanggeunpyo@gmail.com>
2024-07-10 16:50:40 +0900

    [Feature] get_sum_sum 기능

--- calc.py
+++ calc.py
@@ -1,2 +1,6 @@
 class Calc:
-    pass
+    def get_sum_sum(self, a, b, c):
+        if type(a) != int or type(b) != int or type(c) != int:
+            raise AttributeError
+
+        return a + b + c
